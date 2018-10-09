6d9acc114770810c9c982301b500f9794a235da8
connoringold <connoringold@gmail.com>
2018-10-09 15:10:25 +0100

    part 2 & 3 done

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-$app->get('/home', function (){
-  return 'ome';
+$app->get('/home', function ($request, $response){
+  return $this->view->render($response, 'home.twig');
 });
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,26 @@ session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
 $app = new \Slim\App([
     'setting' => [
       'displayErrorDetails' => true,
-    ]
+    ],
 ]);
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
+      'cache' => false,
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+      $container->router,
+      $container->request->getUri()
+    ));
+
+    return $view;
+};
 
 require __DIR__ . '/../app/routes.php';
